13a65ab98da66e4caea573a2cb46c5225a1c5f07
nafkaard17 <nafard17@gmail.com>
2023-06-04 19:28:03 +0700

    Modif alert status

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <div {{ $attributes->merge(['class' => 'rounded-xl shadow-lg bg-green-400 p-5 font-medium text-md text-green-700 mb-4']) }}>
         {{ $status }}
     </div>
 @endif
