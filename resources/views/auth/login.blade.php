7cb179b6070a32afa7a5325a0df4fe08936bf49e
nafkaard17 <nafard17@gmail.com>
2023-06-06 17:07:56 +0700

    Mengubah view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,11 +47,12 @@
             <div class="flex content-center items-center justify-center h-full">
                 <div class="w-full lg:w-4/12 px-4">
                     <div
-                        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+                        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gradient-to-tr from-sky-300 border-0"
                     >
                         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-                            <div class="text-blueGray-400 text-center my-12 font-bold">
-                                <h1 class="text-3xl">Login</h1>
+                            <div class="text-sky-950 text-center my-12 font-bold">
+                                <x-application-logo class="block m-auto h-28 w-auto fill-current text-gray-800"/>
+                                <h1 class="mt-4 text-3xl">Login</h1>
                             </div>
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
@@ -84,7 +85,7 @@
                                         >Remember me</span
                                         ></label
                                     >
-                                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                                    <a class="underline text-sm hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                                         {{ __('Belum punya akun?') }}
                                     </a>
                                 </div>
@@ -114,91 +115,10 @@
                 </div>
             </div>
         </div>
-        <footer class="absolute w-full bottom-0 bg-blueGray-800 pb-6">
-            <div class="container mx-auto px-4">
-                <hr class="mb-6 border-b-1 border-blueGray-600" />
-                <div
-                    class="flex flex-wrap items-center md:justify-between justify-center"
-                >
-                    <div class="w-full md:w-4/12 px-4">
-                        <div
-                            class="text-sm text-white font-semibold py-1 text-center md:text-left"
-                        >
-                            Copyright © <span id="get-current-year"></span>
-                            <a
-                                href="https://www.creative-tim.com?ref=njs-login"
-                                class="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
-                            >Creative Tim</a
-                            >
-                        </div>
-                    </div>
-                    <div class="w-full md:w-8/12 px-4">
-                        <ul
-                            class="flex flex-wrap list-none md:justify-end justify-center"
-                        >
-                            <li>
-                                <a
-                                    href="https://www.creative-tim.com?ref=njs-login"
-                                    class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                                >Creative Tim</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="https://www.creative-tim.com/presentation?ref=njs-login"
-                                    class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                                >About Us</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="http://blog.creative-tim.com?ref=njs-login"
-                                    class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                                >Blog</a
-                                >
-                            </li>
-                            <li>
-                                <a
-                                    href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-login"
-                                    class="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                                >MIT License</a
-                                >
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
     </section>
 </main>
 </body>
 <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
-<script>
-    /* Make dynamic date appear */
-    (function () {
-        if (document.getElementById("get-current-year")) {
-            document.getElementById("get-current-year").innerHTML =
-                new Date().getFullYear();
-        }
-    })();
-    /* Function for opning navbar on mobile */
-    function toggleNavbar(collapseID) {
-        document.getElementById(collapseID).classList.toggle("hidden");
-        document.getElementById(collapseID).classList.toggle("block");
-    }
-    /* Function for dropdowns */
-    function openDropdown(event, dropdownID) {
-        let element = event.target;
-        while (element.nodeName !== "A") {
-            element = element.parentNode;
-        }
-        Popper.createPopper(element, document.getElementById(dropdownID), {
-            placement: "bottom-start"
-        });
-        document.getElementById(dropdownID).classList.toggle("hidden");
-        document.getElementById(dropdownID).classList.toggle("block");
-    }
-</script>
 </html>
 
 
